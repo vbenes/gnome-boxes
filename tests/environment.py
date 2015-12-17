0ad7f7fb94d9a4f3d85e566ca6b9415b57824b75
Vladimir Benes <vbenes@redhat.com>
2015-12-17 16:22:51 +0100

    tests: action delay increase to 0.5 from 0.3

--- tests/environment.py
+++ tests/environment.py
@@ -149,7 +149,7 @@ def before_all(context):
         config.logDebugToStdOut = False
         config.typingDelay = 0.1
         config.childrenLimit = 500
-        config.actionDelay = 0.3
+        config.actionDelay = 0.5
 
         # Include assertion object
         context.assertion = dummy()
